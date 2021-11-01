52049d2ec661d955ebb758d8a16ea637b533a9eb
Matt78251 <mattwilson78251@gmail.com>
2021-11-01 10:23:04 -0500

    Matthew Commit, added the timer.

--- GroupProject/src/application/controller/GenericController.java
+++ GroupProject/src/application/controller/GenericController.java
@@ -1,9 +1,14 @@
 package application.controller;
 
+import java.util.Timer;
+import java.util.TimerTask;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
 import javafx.fxml.FXML;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class GenericController {
 	
@@ -17,6 +22,21 @@ public class GenericController {
     private ProgressBar pointComp;
     
     public int pointVal = 2;
+    
+    @FXML
+    private Label timerLabel;
+    
+    @FXML
+    private TextField enteredMinutes;
+    @FXML
+    private TextField enteredSeconds;
+    
+    private Timer myTimer;
+    public int seconds = 0;
+    public int minutes = 0;
+    
+    
+    
 
     @FXML
     void addPointTeamOne(ActionEvent event) {
@@ -44,4 +64,86 @@ public class GenericController {
     		pointComp.setProgress( (double)t1Points/(t1Points+t2Points) );
     	}
     }
+    
+    
+    @FXML
+    void startTime(ActionEvent event) {
+    	
+    	//
+    	//PLEASE PRESS STOP ON THE TIMER IF ITS NOT FULLY RUN OUT
+    	//
+    	//getting info from text fields, checking if empty or not
+    	if(enteredMinutes.getText().trim().isEmpty() && enteredSeconds.getText().trim().isEmpty())
+        {
+        	minutes = 0;
+        	seconds = 0;
+        }
+        
+    	else if(enteredSeconds.getText().trim().isEmpty())
+        {
+        	seconds = 0;
+        	minutes = Integer.parseInt(enteredMinutes.getText().trim());
+        }
+    	
+    	else if(enteredMinutes.getText().trim().isEmpty())
+        {
+        	minutes = 0;
+        	seconds = Integer.parseInt(enteredSeconds.getText().trim());
+        }
+    	
+    	
+    	else {
+    		seconds = Integer.parseInt(enteredSeconds.getText().trim());
+            minutes = Integer.parseInt(enteredMinutes.getText().trim());
+    	}
+        
+        
+        //making a timer
+        myTimer = new Timer();
+        myTimer.scheduleAtFixedRate(new TimerTask(){
+        	//running timer
+        	public void run() {
+        		Platform.runLater(() -> {
+        			//if time = 00:00, stops timer
+        			if((minutes == 0) && (seconds == 0))
+        			{
+        				myTimer.cancel();
+        			}
+        			//int to string for textfield
+        			String str = String.valueOf(seconds);
+        			String str2 = String.valueOf(minutes);
+        			
+        			//adding a zero in front of digits less than 10
+        			if(seconds < 10)
+        			{
+        				str = String.valueOf("0" + seconds);
+        			}
+        			if(minutes < 10)
+        			{
+        				str2 = String.valueOf("0" + minutes);
+        			}
+        			
+        			//printing time to label
+        			timerLabel.setText(str2 + ":" + str);
+        			
+        			//decrementing time
+        			if((minutes != 0) && (seconds == 0))
+        			{
+        				seconds = 60;
+        				minutes--;
+        			}
+        			
+        			seconds--;
+        			
+        		});
+        	}
+        }, 0, 1000);
+
+    }
+
+    @FXML
+    void stopTime(ActionEvent event) {
+    	myTimer.cancel();
+    }
+
 }
