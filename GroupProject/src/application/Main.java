62ac8c1bef123a5e0b85dc9e01b920f913b042b6
wrinklykong <thatonejwjguy@gmail.com>
2021-11-04 15:03:26 -0500

    Test hello

--- GroupProject/src/application/Main.java
+++ GroupProject/src/application/Main.java
@@ -11,6 +11,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
+//test
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
